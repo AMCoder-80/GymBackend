deed55511504f961cf180c6d61a548dbd9b2b115
Alireza Mortezaei <alirezamortezaei50@gmail.com>
2024-06-14 14:19:21 +0330

    add cors-header module

--- core/settings.py
+++ core/settings.py
@@ -43,9 +43,12 @@ INSTALLED_APPS = [
     "base.apps.BaseConfig",
     # Third party modules
     "rest_framework_simplejwt",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -156,3 +159,5 @@ CACHES = {
         }
     }
 }
+
+CORS_ALLOW_ALL_ORIGINS=True
